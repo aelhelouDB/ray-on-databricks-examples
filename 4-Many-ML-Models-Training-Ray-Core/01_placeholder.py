8206c83b2580bd5dfdee051016b33871e7384c6a
puneet-jain159 <puneet-jain159>
2025-01-20 01:30:18 +0000

    commit to update notebooks with UC

--- 4-Many-ML-Models-Training-Ray-Core/01_placeholder.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC Get inspiration from [this repo](https://github.com/puneet-jain159/DAIS_Forecasting_using_udf_ray/tree/main)
-# MAGIC
-# MAGIC #TO-DO
-
-# COMMAND ----------
-
-
